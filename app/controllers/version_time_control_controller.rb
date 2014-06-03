d28f69ab27cbe18cff99576f74f5cb74a04a0b4b
misterfifi1 <figiel.mathieu.info@gmail.com>
2014-06-03 22:54:30 +0200

    better conception + add estimated time to version option

--- app/controllers/version_time_control_controller.rb
+++ app/controllers/version_time_control_controller.rb
@@ -6,9 +6,7 @@ class VersionTimeControlController < ApplicationController
     @custom_field =  Setting.plugin_version_time_control[:contract_field]
     @hours_field = Setting.plugin_version_time_control[:hours_field]
     if @custom_field != "" && @hours_field != ""
-      @issues = "SELECT p.name as projectname, v.name, ROUND(sum(t.hours),2) as spent_time, v.id as version_id,
-      ROUND(sum(i.estimated_hours)/count(t.id),2) as estimaed_time,
-      sum(t.hours) as time_spent, c.id as customid
+      @sql = "SELECT p.name as projectname, v.name, ROUND(sum(t.hours),2) as spent_time, c.id as customid, v.id as version
       FROM   time_entries as t, versions as v,
              issues as i, projects as p,
              custom_values as c
@@ -18,8 +16,9 @@ class VersionTimeControlController < ApplicationController
          and v.`status` = 'open'
          and v.id = c.customized_id
          and c.value = 1
-         and c.custom_field_id = 1 GROUP BY t.issue_id"
-      @issues = Version.find_by_sql([@issues])
+         and c.custom_field_id = " + @custom_field +"
+         GROUP BY v.id"
+      @issues = Version.find_by_sql([@sql])
       @liste = []
       for @time in @issues
         @values = CustomValue.where(:customized_id => @time[:customid],:custom_field_id => @hours_field).all
@@ -32,10 +31,12 @@ class VersionTimeControlController < ApplicationController
               {
                         :projectname => @time[:projectname],
                         :name => @time[:name],
+                        :version => @time[:version],
                         :spent_time => @time[:spent_time],
                         :percent => @percent,
+                        :percentrnd => sprintf( "%.2f", @percent.to_f ),
                         :estimaed_time => @val.to_f,
-                        :percenttofinish => 100 - @percent.to_f,
+                        :percenttofinish => 100 - @percent,
                     }
           )
         end
